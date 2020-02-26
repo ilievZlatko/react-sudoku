bda65a86982eb9bed3986abff040ded02bdbf702
Zlatko Iliev <zlatko.iliev@boughtbymany.com>
2020-02-26 09:11:38 +0000

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Content, Title, Card, Grid } from './components'
+import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
+
+unregister()
