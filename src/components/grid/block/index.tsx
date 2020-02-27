fec783bd5b01db25fdcba7936732518f03202e0e
Zlatko Iliev <zlatko.iliev@boughtbymany.com>
2020-02-27 09:19:44 +0000

    write shuffle algorithm and unit tests

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`} />
+  return <Container data-cy={`block-${rowIndex}-${colIndex}`}></Container>
 }
 
 export default Block
---
+++ src/utils/fill-grid/index.ts
@@ -0,0 +1,43 @@
+import { GRID, NUMBERS } from 'typings'
+import { shuffle } from 'utils'
+
+const gridExample = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+/**
+ * A backtracking/recursive function
+ * to check all the possible combinations
+ * of numbers until a solution is found
+ * @param grid 9x9 sudoku grid
+ */
+function fillGrid(grid: GRID) {
+  let row = 0
+  let col = 0
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9)
+    col = i % 9
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers)
+      // Do stuff
+      // recursive
+      break
+    }
+
+    grid[row][col] = 0
+  }
+}
+
+export default fillGrid
